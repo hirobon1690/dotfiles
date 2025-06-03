5164903a46e884ac7798daff3ef60b2c4792757b
hirobon1690 <58695125+hirobon1690@users.noreply.github.com>
2025-06-03 23:04:42 +0900

    Create install-essential.sh

---
+++ install-essential.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+echo "Installing essential packages..."
+sudo apt update
+sudo apt install -y build-essential git curl wget zsh
+
+echo "code code/add-microsoft-repo boolean true" | sudo debconf-set-selections
+sudo apt update
+sudo apt install -y code
+
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/googlechrom-keyring.gpg] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/googlechrom-keyring.gpg
+sudo apt update
+sudo apt install -y google-chrome-stable
+
+type -p curl >/dev/null || sudo apt install curl -y
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sed -i '/^plugins=(/ { /zsh-autosuggestions/! s/)/ zsh-autosuggestions)/; /zsh-syntax-highlighting/! s/)/ zsh-syntax-highlighting)/ }' ~/.zshrc
+echo "ZSH_AUTOSUGGEST_STRATEGY=(completion history)" >> ~/.zshrc
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' ~/.zshrc

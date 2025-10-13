2f633c15290d6dc883fdcadb13e3efef31562c75
hirobon1690 <58695125+hirobon1690@users.noreply.github.com>
2025-10-13 12:44:25 +0900

    Update install-essential.sh

--- install-essential.sh
+++ install-essential.sh
@@ -8,7 +8,8 @@ fi
 
 echo "Installing essential packages..."
 sudo apt update
-sudo apt install -y build-essential git curl wget zsh tmux terminator unzip python3-nautilus flatpak
+sudo apt install -y build-essential git curl wget zsh tmux terminator unzip python3-nautilus flatpak python3-pip python3-venv
+wget -qO- https://astral.sh/uv/install.sh | sh
 wget https://raw.githubusercontent.com/hirobon1690/dotfiles/refs/heads/main/.tmux.conf
 mkdir -p ./.config/terminator
 wget -O ~/.config/terminator/config https://raw.githubusercontent.com/hirobon1690/dotfiles/refs/heads/main/terminator/config
